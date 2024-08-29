cc7fd1be729edcc0f3aea2d74858cfc7d04207c5
rahul <rahulmuddasani143@.com>
2024-08-29 14:58:32 +0530

    Fix react-scripts not found issue

---
+++ src/Components/Products/Wishlist_Shimmer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Wishlist_Shimmer() {
+  return (
+    <div>
+      <div
+        className="col-lg-3 col-md-6 col-sm-12 border bg-dark d-flex flex-row"
+        style={{ height: "600px;" }}
+      >
+        {" "}
+        <h1>Hello</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Wishlist_Shimmer
